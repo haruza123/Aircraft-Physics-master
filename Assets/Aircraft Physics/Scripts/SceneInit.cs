484a03b0c4b3bb6a7393b62098ae440abc5d103e
haruza123 <haruzakatana@gmail.com>
2024-12-09 20:04:51 +0700

    perubahan

---
+++ Assets/Aircraft Physics/Scripts/SceneInit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInit : MonoBehaviour
+{
+
+    public GameObject[] aircrafts;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(aircrafts[PlayerPrefs.GetInt("SelectedAircraft", 0)]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
