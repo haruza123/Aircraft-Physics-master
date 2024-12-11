86549e934278d257ef735637823ca2d6f2696fc5
haruza123 <haruzakatana@gmail.com>
2024-12-11 12:55:23 +0700

    sound, ganti pesawat

---
+++ Assets/Aircraft Physics/Scripts/EngineSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineSound : MonoBehaviour
+{
+    AudioSource aus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aus = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aus.pitch =MobileAirplaneController.instance.slider.value;
+    }
+}
